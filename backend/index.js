f0542f4daed27ec2751b4efe7fc937fafbf6a688
mohamedmdi <mohamed.mdi.sg@gmail.com>
2026-05-15 01:55:57 +0100

    fix: fixing the import of nanoid module

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
-const { nanoid } = require("nanoid");
+import { nanoid } from 'nanoid';
 require("dotenv").config();
 
 const app = express();
